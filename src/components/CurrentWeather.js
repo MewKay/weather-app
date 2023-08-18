0727f7036a1810430774b58d2529cd57d29e75e9
MewKay <125735249+MewKay@users.noreply.github.com>
2023-08-18 23:34:28 +0300

    Add format to currentWeather time

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { format } from "date-fns";
 
 const epochToDate = function convertUnixTimeStampToDateObject(timeEpoch) {
@@ -8,19 +9,26 @@ const getFormattedDate = function formatDateToConvenientPattern(date) {
   return format(date, "EEEE do MMMM yyyy");
 };
 
+const getFormattedTime = function formatDateToGetTheTimeHourAndMinute(date) {
+  return format(date, "hh:mm aaa");
+};
+
 const CurrentWeather = function createCurrentWeatherObject(data) {
   const {
     location,
     current: { condition, ...current },
   } = data;
   const { name: cityName, country: countryName } = location;
+
   const localDate = epochToDate(location.localtime_epoch);
   const date = getFormattedDate(localDate);
+  const time = getFormattedTime(localDate);
 
   return {
     cityName,
     countryName,
     date,
+    time,
   };
 };
 
