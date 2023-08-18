443ed0da0352a891ece6632e126b90f8461e8493
MewKay <125735249+MewKay@users.noreply.github.com>
2023-08-19 01:31:48 +0300

    Delete getCurrentWeather module
    getCurrentWeather module will be deleted in favor of getForecastWeather to fetch the data from the api.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import CurrentWeather from "./components/CurrentWeather";
-import getCurrentWeather from "./components/getCurrentWeather";
 import getForecastWeather from "./components/getForecastWeather";
 import "./styles/reset.css";
 import "./styles/style.css";
 
 const cityName = "Antananarivo";
-const weatherData = await getCurrentWeather(cityName);
-const futureData = await getForecastWeather(cityName);
+const weatherData = await getForecastWeather(cityName);
 const myWeather = CurrentWeather(weatherData);
 
 console.log(weatherData);
-console.log(futureData);
